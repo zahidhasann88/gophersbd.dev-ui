aa850a31a8f1eff615834fcded03a84d7f5eb2f4
Omran Jamal <o.jamal97@gmail.com>
2024-12-22 23:57:41 +0600

    sponsor logo added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				oswald: 'Oswald, sans-serif',
+				mono: '"JetBrains Mono", monospace'
+			},
+			boxShadow: {
+				sponsor: '12px 12px #2d5d73'
+			}
+		},
 	},
 	plugins: [],
 }
